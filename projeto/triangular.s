a81e4ea5dc2f8c0f4c5b160d27869b1e6d6a481d
Guilherme Zampronio <guizampronio@gmail.com>
2023-12-06 21:15:25 -0300

    Adicionado mais comentarios ao codigo do triangular.s

--- projeto/triangular.s
+++ projeto/triangular.s
@@ -51,7 +51,7 @@ CALC_TRIANGULAR:
 
   mov r13, r0 # inicia acumulador com 0
 
-  # limpar todos os diplays
+  # limpar todos os diplays para mostrar um novo número
   stbio r0, 0(r12)
   stbio r0, 1(r12)
   stbio r0, 2(r12)
@@ -62,50 +62,50 @@ CALC_TRIANGULAR:
   stbio r0, 2(r17)
   stbio r0, 3(r17)
 
-  movi r8, 1 # registrador para ficar vendo se o r9 lido pelo switch vai ser igual a 1
+  movi r8, 1 # registrador para ficar vendo se o r9 lido pelo switch vai ser igual a 1 e parar de calcular o triangular
 
   LOOP_TRIANGULAR:
-    beq r9, r8, EXIT_LOOP_TRIANGULAR
-    add r13, r13, r9
+    beq r9, r8, EXIT_LOOP_TRIANGULAR 
+    add r13, r13, r9 # r13 é o acumulador que vai receber o valor triangular
     addi r9, r9, -1
     br LOOP_TRIANGULAR
 
   EXIT_LOOP_TRIANGULAR:
-    addi r13, r13, 1 # precisa somar 1 no final pq ele sai do loop quando r9 for 1 ( mas ele não estava somando, logo soma quando sair do loop)
+    addi r13, r13, 1 # precisa somar 1 no final pq ele sai do loop quando r9 for 1 ( mas ele não estava somando, logo soma 1 quando sair do loop para obter o valor correto)
 
-    movi r8, 10
-    movi r10, 0
+    movi r8, 10 # r8 para fazer a divisão por 10
+    movi r10, 0 # acumulador para ver quantos digitos/display ele já colocou um número
 
     # r14 = resultado da divisao
     # r15 = resto da divisao
 
      mov r14, r13 # inicializo o r14 com o valor
-     movi r16, 0
+     movi r16, 0 # r16 vai receber o quociente da divisão da iteracao 
 
      LOOP_DISPLAY: # a ideia eh montar um loop para exibir cada numero
        beq r14, r0, EXIT_LOOP_DISPLAY # se o resultado da divisao for 0, para o loop
     
-       div r16, r14, r8 # calcula o resultado da divisao (r14/10)
-       mul r15, r16, r8
-       sub r15, r14, r15 # pega o resto
+       div r16, r14, r8 # lógica para pegar o resto
+       mul r15, r16, r8 # lógica para pegar o resto
+       sub r15, r14, r15 # pega o resto no r15
        
-       mov r14, r16
-       movi r16, 4 # (comparar r10 sendo maior ou igual a 4 para pegar DISPLAY_7_SEG_BASE1 ou DISPLAY_7_SEG_BASE2 )
+       mov r14, r16 # coloca o quociente em r14 para a próxima iteracao ele ser o dividendo
+       movi r16, 4 # comparar r10 sendo maior ou igual a 4 para pegar DISPLAY_7_SEG_BASE1 ou DISPLAY_7_SEG_BASE2 por isso colocamos r16 com 4 aqui
 
        #Xo dígito
        add r9, r15, r11 # pega o endereço do item na lista
        ldbio r9, 0(r9) # valor correspondente da tabela
 
-       bge r10, r16, SET_BASE_DISPLAY2
+       bge r10, r16, SET_BASE_DISPLAY2 # se o r10 mencionado anteriormente for maior igual a 4 ele precisa usar os 4 ultimos display de 7seg então ele vai para SET_BASE_DISPLAY2, se não ele segue para acender os 4 primeiros
        
-       add r13, r10, r12 # calculo o endereço dinamico da tabela, r12 é a base do 7seg
-       stb r9, 0(r13) # define o valor de HEX0 como o valor obtido da tabela
-       addi r10, r10, 1
-       br LOOP_DISPLAY
+       add r13, r10, r12 # calcula o endereço dinamico da tabela, r12 é a base do 7seg para usar abaixo e acender o HEX(x)
+       stb r9, 0(r13) # define o valor de HEX(x) como o valor obtido da tabela, logo coloca no display x o valor correto
+       addi r10, r10, 1 # adiciona ao acumulador r10
+       br LOOP_DISPLAY # volta para o inicio do loop para fazer o próximo digito/display
 
-       SET_BASE_DISPLAY2:
-        subi r16, r10, 4
-        add r13, r16, r17
+       SET_BASE_DISPLAY2: # se cair aqui ele precisa acender um dos 4 ultimos displays, lógica praticamente identica só muda o endereço base do display
+        subi r16, r10, 4 # Aqui o resultado da subtração do acumulador r10 - 4 em r16. Isso é feito para saber qual x registrador dos 4 ultimos ele precisa inserir o numero
+        add r13, r16, r17 # proximas 3 linhas logica normal para inserir/acender o display.
         stb r9, 0(r13)
         addi r10, r10, 1
         br LOOP_DISPLAY
